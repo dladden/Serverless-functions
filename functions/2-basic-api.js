ca74f997ca1143a7dffb27f0f2b2d714c119b60f
dladden <dladden77@gmail.com>
2022-10-05 16:50:25 -0400

    header access set to all

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,12 +2,18 @@
 //Instead of housing the raw data in the function file we can store it in a more acessible file at our choosing
 //and export it out.
 
+//Additionally, you can create your own header which allows you to change information about the status of the request
+//In this instance we set access to all '*' with the 'Access-Control-Allow-Origin':'*',
+
 const items = require("../assets/data");
 
 exports.handler = async (event, context, cb) => {
   //   console.log(event);
   //   console.log(context);
   return {
+    Headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
